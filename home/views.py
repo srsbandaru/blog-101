e02e214fd62b9160b296fd6469818f0afde70e02
srsbandaru <86214783+srsbandaru@users.noreply.github.com>
2024-04-06 17:15:42 +1100

    Home Page

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+class IndexPage(View):
+    template_name = "home/index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+    
